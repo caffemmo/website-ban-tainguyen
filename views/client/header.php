6f12148a261c79f8a7dbc860df8af86f1feee180
caffemmo <caffemmo.com@gmail.com>
2026-07-01 12:58:16 +0700

    Revert "Use theme button typography"
    This reverts commit 1904561aca0d5c6ef4065eec1789850fe6602c95.

--- views/client/header.php
+++ views/client/header.php
@@ -96,7 +96,7 @@ checkMaintenance();
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   <?php endif ?>
   <?= $body['header']; ?>
-  <link rel="stylesheet" href="<?= BASE_URL('mod/css/main.css?v=30'); ?>">
+  <link rel="stylesheet" href="<?= BASE_URL('mod/css/main.css?v=29'); ?>">
   <script src="<?= base_url('mod/js/main.js?v=2'); ?>"></script>
   <?= $CMSNT->site('javascript_header'); ?>
 
