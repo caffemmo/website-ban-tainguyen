5d424a115e5354b246494321e38bc5c1ebc93be0
caffemmo <caffemmo.com@gmail.com>
2026-08-03 18:58:47 +0700

    Polish header selectors and homepage spacing

--- views/client/nav.php
+++ views/client/nav.php
@@ -116,6 +116,7 @@ $serviceCatalog = caffemmo_service_catalog();
                                             <?= getLanguage() == $lang['lang'] ? 'selected' : ''; ?>><?= $lang['lang']; ?></option>
                                     <?php endforeach; ?>
                                 </select>
+                                <span class="header-select-arrow" aria-hidden="true"><i class="fa-solid fa-chevron-down"></i></span>
                             <?php elseif ($CMSNT->site('language_type') == 'gtranslate'): ?>
                                 <?= $CMSNT->site('gtranslate_script'); ?>
                             <?php endif ?>
@@ -128,6 +129,7 @@ $serviceCatalog = caffemmo_service_catalog();
                                     </option>
                                 <?php endforeach; ?>
                             </select>
+                            <span class="header-select-arrow" aria-hidden="true"><i class="fa-solid fa-chevron-down"></i></span>
                         </div>
                     </div>
                 </div>
