51c8b248ade672f20e38026b8ed7e51d918fb61c
caffemmo <caffemmo.com@gmail.com>
2026-08-04 20:30:23 +0700

    Remove Netflix shortcut from Up Tich Xanh

--- views/client/up-tich-xanh.php
+++ views/client/up-tich-xanh.php
@@ -19,7 +19,6 @@ $services = [];
 foreach ($serviceCatalog['up-tich-xanh']['items'] as $catalogItem) {
     $services[$catalogItem['key']] = $catalogItem;
 }
-$netflixService = $serviceCatalog['streaming']['items'][0] ?? null;
 
 $service = isset($_GET['service']) && isset($services[$_GET['service']]) ? $_GET['service'] : 'get-link';
 $currentService = $services[$service];
@@ -70,13 +69,6 @@ require_once __DIR__ . '/nav.php';
                 <i class="fa-solid fa-arrow-up-right-from-square up-service-tab-arrow" aria-hidden="true"></i>
             </a>
         <?php endforeach; ?>
-        <?php if (is_array($netflixService)): ?>
-            <a class="up-service-tab up-service-tab--<?= htmlspecialchars($netflixService['tone'], ENT_QUOTES, 'UTF-8'); ?>" href="<?= htmlspecialchars($netflixService['url'], ENT_QUOTES, 'UTF-8'); ?>">
-                <span class="up-service-tab-icon"><i class="<?= htmlspecialchars($netflixService['icon'], ENT_QUOTES, 'UTF-8'); ?>" aria-hidden="true"></i></span>
-                <span><strong><?= __($netflixService['label']); ?></strong><small><?= __($netflixService['short']); ?></small></span>
-                <i class="fa-solid fa-arrow-up-right-from-square up-service-tab-arrow" aria-hidden="true"></i>
-            </a>
-        <?php endif; ?>
     </section>
 
     <div class="up-history-link-row">
