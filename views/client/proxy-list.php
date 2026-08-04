abe4ed06d90fddb34cd7ed7767a634d3b0dfc42f
caffemmo <caffemmo.com@gmail.com>
2026-08-04 16:38:40 +0700

    Place proxy header description below title

--- views/client/proxy-list.php
+++ views/client/proxy-list.php
@@ -13,7 +13,7 @@ $body = [
     'desc' => __('Quản lý proxy đã mua, thông tin đăng nhập và thời hạn.'),
     'keyword' => 'proxy của tôi, quản lý proxy'
 ];
-$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/proxy.css?v=11') . '">';
+$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/proxy.css?v=12') . '">';
 $body['footer'] = '<script src="' . BASE_URL('mod/js/proxy.js?v=7') . '"></script>';
 
 require_once __DIR__ . '/header.php';
