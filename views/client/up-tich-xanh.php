fce6f9960a645b91dc3c04dccecd0b92b83eafc1
caffemmo <caffemmo.com@gmail.com>
2026-08-04 16:20:04 +0700

    Remove dark borders from Up history table

--- views/client/up-tich-xanh.php
+++ views/client/up-tich-xanh.php
@@ -29,7 +29,7 @@ $body = [
     'desc' => __('Dịch vụ Get Link Facebook, Up tích Facebook và Up tích Instagram.'),
     'keyword' => 'get link facebook, up tích xanh, up tích facebook, up tích instagram'
 ];
-$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/up-tich-xanh.css?v=13') . '">';
+$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/up-tich-xanh.css?v=14') . '">';
 $body['footer'] = '<script src="' . BASE_URL('mod/js/up-tich-xanh.js?v=5') . '"></script>';
 
 require_once __DIR__ . '/header.php';
