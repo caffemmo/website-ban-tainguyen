658e6cca4753bb23cc9c25466c10c7d94a90d244
caffemmo <caffemmo.com@gmail.com>
2026-08-04 17:00:19 +0700

    Match proxy page header sizing

--- views/client/proxy-renew.php
+++ views/client/proxy-renew.php
@@ -13,7 +13,7 @@ $body = [
     'desc' => __('Gia hạn nhiều proxy cùng lúc và bật tự động gia hạn khi cần.'),
     'keyword' => 'gia hạn proxy'
 ];
-$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/proxy.css?v=14') . '">';
+$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/proxy.css?v=15') . '">';
 $body['footer'] = '<script src="' . BASE_URL('mod/js/proxy.js?v=7') . '"></script>';
 
 require_once __DIR__ . '/header.php';
