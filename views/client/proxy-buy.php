9c86b93445cbe0ae406b7be5cd4856fedf1da48a
caffemmo <caffemmo.com@gmail.com>
2026-08-06 01:21:32 +0700

    Bust proxy guide stylesheet cache

--- views/client/proxy-buy.php
+++ views/client/proxy-buy.php
@@ -17,7 +17,7 @@ $body = [
     'desc' => __('Mua proxy chính hãng với cấu hình rõ ràng và quản lý tập trung.'),
     'keyword' => 'proxy, mua proxy, proxy premium'
 ];
-$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/proxy.css?v=27') . '">';
+$body['header'] = '<link rel="stylesheet" href="' . BASE_URL('mod/css/proxy.css?v=28') . '">';
 $body['footer'] = '<script src="' . BASE_URL('mod/js/proxy.js?v=9') . '"></script>';
 
 require_once __DIR__ . '/header.php';
